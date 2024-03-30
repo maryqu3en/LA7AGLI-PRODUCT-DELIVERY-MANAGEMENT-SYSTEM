4f89b4135005cc4b72b3585386e88bef5bb6e524
meriem soubih <soubih.meriem@gmail.com>
2024-03-30 09:25:52 +0100

    failed attempt to act organized

--- Controllers/connexion.php
+++ Controllers/connexion.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "University";
+$dbname = "Livraison";
 
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
