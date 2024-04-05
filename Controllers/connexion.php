6280f18aa79f1195d6b4dafee6b79d1fc172c108
meriem soubih <soubih.meriem@gmail.com>
2024-04-05 12:30:02 +0200

    redesigned database :p

--- Controllers/connexion.php
+++ Controllers/connexion.php
@@ -4,5 +4,8 @@ $username = "root";
 $password = "";
 $dbname = "Livraison";
 
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
--- Model/initUser.sql
+++ Model/initUser.sql
@@ -1,6 +1,5 @@
 USE Livraison;
-INSERT INTO user (nom_user, prenom_user, password_user, email_user, produit, prix)
-VALUES ('Soubih', 'Meriem', 'stay', 'meriem@meriem.meriem', 'Notebook', '500.00');
 
-INSERT INTO user (nom_user, prenom_user, password_user, email_user, produit, prix)
-VALUES ('Zellal', 'Houda', 'green', 'houda@houda.houda', 'Candle', '300.00');
+INSERT INTO user (nom_user, prenom_user, password_user, email_user, produit, prix) VALUES 
+('Soubih', 'Meriem', 'stay', 'meriem@meriem.meriem', 'Notebook', '500.00'), 
+('Zellal', 'Houda', 'green', 'houda@houda.houda', 'Candle', '300.00');
